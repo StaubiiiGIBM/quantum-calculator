55e24d623db3d3897a6a8f49f85382f5d42b1f4a
StaubiiiGIBM <leon.schaub@bbzbl-it.ch>
2025-12-08 08:34:28 +0100

    Add supabase environment config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  supabaseUrl: 'https://exhkrdovjijygecdwslt.supabase.co',
+  supabaseKey: 'sb_publishable_fVncteoP8A5_gE5TT7Lyxw_LL0KpNEc'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  supabaseUrl: 'https://exhkrdovjijygecdwslt.supabase.co',
+  supabaseKey: 'sb_publishable_fVncteoP8A5_gE5TT7Lyxw_LL0KpNEc'
 };
 
 /*
