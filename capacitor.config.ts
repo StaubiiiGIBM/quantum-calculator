28de38870f3975927ca011c99446732787e4fa87
StaubiiiGIBM <leon.schaub@bbzbl-it.ch>
2025-12-08 16:00:12 +0100

    Install Cordova-res

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,16 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'quantum-calculator',
-  webDir: 'www'
+  webDir: 'www',
+  cordova: {
+    preferences: {
+      ScrollEnabled: 'false',
+      'android-minSdkVersion': '22',
+      BackupWebStorage: 'none',
+      DisallowOverscroll: 'true',
+      Orientation: 'portrait'
+    }
+  }
 };
 
 export default config;
